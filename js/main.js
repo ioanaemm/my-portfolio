dbaad06f85424767bbaa5209ea536866c0167371
ioanaemm <miteleaioana@gmail.com>
2017-05-30 16:19:05 +0100

    styled map and added a contact page

--- js/main.js
+++ js/main.js
@@ -1,28 +1,103 @@
 
-// var locations = {
-//   "schools": [
-//     {
-//       name: "National College 'Cuza Voda'",
-//       city: "Husi, Vaslui, Romania"
-//     },
-//     {
-//       name: "Faculty of Psychology and Educational Sciences, University of Bucharest",
-//       city: "Bucharest, Romania"
-//     }],
-//   "workplaces": [
-//     {
-//       name: "Digital Monkeyz",
-//       city: "Bucharest, Romania"
-//     },
-//     {
-//       name: "Opearlo - The Voice Design Agency",
-//       city: "London, United Kingdom"
-//     }
-//   ]
-// };
-
-
 function initMap() {
+  var styles = [
+    {
+        featureType: "water",
+        stylers: [
+          { color: "#f8e6dc" }
+        ]
+      },
+      {
+        featureType: "administrative",
+        elementType: "labels.text",
+        stylers: [
+          { color: "#7e0030" },
+          { weight: 4 }
+        ]
+      },
+      {
+        featureType: "administrative",
+        elementType: "labels.text.stroke",
+        stylers: [
+          { color: "#fff" },
+
+        ]
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry.stroke",
+        stylers: [
+          { color: "#ffffff" }
+        ]
+      },
+      {
+        featureType: "transit.station",
+        stylers: [
+          { weight: 9 },
+          { hue: "#e85113" }
+        ]
+      },
+      {
+        featureType: "road.highway",
+        elementType: "labels.icon",
+        stylers: [
+          { visibility: "off" }
+        ]
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.stroke",
+        stylers: [
+          { lightness: 100 }
+        ]
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.fill",
+        stylers: [
+          { lightness: -100 }
+        ]
+      },
+      {
+        featureType: "poi",
+        elementType: "geometry",
+        stylers: [
+          { visibility: "on" },
+          { color: "#f0e4d3" }
+        ]
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry.fill",
+        stylers: [
+          {color: "#ffffff"},
+          { lightness: -25 }
+        ]
+      },
+      {
+        featureType: "road.arterial",
+        elementType: "geometry.fill",
+        stylers: [
+          {color: "#dddddd"}
+        ]
+      },
+      {
+        featureType: "road.local",
+        elementType: "geometry.fill",
+        stylers: [
+          {color: "#FFF5CB"}
+        ]
+      },
+      {
+        featureType: "landscape.natural",
+        stylers: [
+          {color: "#ffdbc5"}
+        ]
+      }
+
+  ];
+
+
   var map;
   var bounds = new google.maps.LatLngBounds();
   var markers = [
@@ -30,28 +105,24 @@ function initMap() {
     ['Bucharest, Romania', 44.541407, 26.225575],
     ['London, United Kingdom',51.5073509, -0.1277583]
   ];
+
+  var defaultIcon = "img_src/fixed/location-marker.png";
+
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 51.5073509, lng: -0.1277583},
     scrollwheel: false,
-    zoom: 4
+    zoom: 4,
+    styles: styles
   });
 
-  /*for(var i = 0; i < markers.length; i++) {
-    var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
-    console.log(position);
-    bounds.extend(position);
-    var marker = new google.maps.Marker({
-      position: position,
-      map: map,
-      title: markers[i][0]
-    });
-  }*/
+
 
   markers.forEach(function(marker){
     var position = new google.maps.LatLng(marker[1], marker[2]);
     bounds.extend(position);
     marker = new google.maps.Marker({
       position: position,
+      icon: defaultIcon,
       map: map,
       title: marker[0]
     });
@@ -60,64 +131,5 @@ function initMap() {
 
 
 
-  // function locationFinder() {
-  //   var places = [];
-  //   // locations.schools.forEach(function(school){
-  //   //   places.push(school.city);
-  //   // });
-  //
-  //   for(var i = 0; i < locations.schools.length; i++) {
-  //     // console.log("aa");
-  //     places.push(locations.schools[i].city);
-  //     // console.log(places);
-  //   }
-  //
-  //   // locations.workplaces.forEach(function(workplace){
-  //   //   places.push(workplace.city);
-  //   // });
-  //
-  //   for(var i = 0; i < locations.workplaces.length; i++) {
-  //     // console.log(locations[i]);
-  //     places.push(locations.workplaces[i].city);
-  //     // console.log(places);
-  //   }
-  //   return places;
-  // }
 
-  // function createMarker() {
-    // var markers = locationFinder();
-    // var markers = locations;
-    // console.log('markers', markers);
-    // markers.forEach(function(marker){
-    //   var position = new google.maps.LatLng(marker.schools.city);
-    //   marker = new google.maps.Marker({
-    //     position: position,
-    //     map: map,
-    //     title: marker.schools.name
-    //   });
-    // });
-    // for(var i = 0; i < markers.length; i++) {
-    //   var position = new google.maps.LatLng(marker[i].city)
-    // }
-
-  // }
-  // createMarker();
-
-  // function pinPoster(locations) {
-    // var service = new google.maps.places.PlacesService(map);
-
-    // locations.forEach(function(place){
-      //console.log(place);
-
-      // var request =  {
-      //   query: place
-      // };
-
-    //service.textSearch(request, callback);
-    // });
-  // }
-  // locations = locationFinder();
-
-  // pinPoster(locations);
-  //console.log(locationFinder());
 }
